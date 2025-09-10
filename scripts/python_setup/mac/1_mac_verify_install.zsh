56ddf87436b76a971e8c2228c9bea2f492cdaa53
Kugendran <kugendran@me.com>
2025-09-10 11:58:51 +0100

    modified script for mac install verification

--- scripts/python_setup/mac/1_mac_verify_install.zsh
+++ scripts/python_setup/mac/1_mac_verify_install.zsh
@@ -1,6 +1,12 @@
+# verify Pytorch and MPS libraries
 python - << 'PY'
 import torch
+
+print(f'\nMac Pytorch tests:')
 print("PyTorch:", torch.__version__)
 print("MPS available:", torch.backends.mps.is_available())
 print("MPS built:", torch.backends.mps.is_built())
 PY
+
+# verify dependent libraries
+zsh tests/mac_test_pytorch.zsh
