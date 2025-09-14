298cb94639e470f6eb39c1c684a7d1d116acefe0
Kugendran <kugendran@me.com>
2025-09-14 20:23:35 +0100

    removed unneeded scripts

--- scripts/python_setup/intel_docker/5_sanity.sh
+++
@@ -1,5 +0,0 @@
-docker run --rm -it \
-  --platform=linux/amd64 \
-  -e PATH="/home/app/.local/bin:/usr/local/bin:/usr/bin:/bin" \
-  x86_pytorch:latest \
-  sh -lc 'command -v mkdssp && mkdssp --version || (echo "mkdssp missing"; exit 127)'
