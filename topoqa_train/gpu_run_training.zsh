918aa1af1f8fc8df24b157dd4fead9860ab89bb0
Kugendran <kugendran@me.com>
2025-10-21 16:25:01 +0100

    inference scripts

---
+++ topoqa_train/gpu_run_training.zsh
@@ -0,0 +1,12 @@
+  python train_topoqa.py \
+    --graph_dir graph_data \
+    --train_label_file train.csv \
+    --val_label_file val.csv \
+    --attention_head 8 \
+    --pooling_type "mean" \
+    --batch_size 16 \
+    --accumulate_grad_batches 32 \
+    --learning_rate 0.005 \
+    --num_epochs 200 \
+    --seed 222 \
+    --save_dir ./experiments/lr_0005_222
