bc665f207c35d47db5513f9d7d5e860c67fb0d42
Kugendran <kugendran@me.com>
2025-10-26 08:17:35 +0000

    chore: remove old git helper scripts

--- git_check.zsh
+++
@@ -1,5 +0,0 @@
-git fetch origin
-git rev-list --left-right --count HEAD...origin/main
-git log --oneline HEAD..origin/main
-git diff --name-status HEAD..origin/main
-git diff --stat HEAD..origin/main
--- git_noconflicts.zsh
+++
@@ -1,5 +0,0 @@
-git pull --ff-only
-# git add <your files>          # or: git add -A
-# git commit -m "Your message"
-# git push
-
