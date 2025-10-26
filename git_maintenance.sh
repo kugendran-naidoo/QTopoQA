d53b08f972778016db9fd6eda9e287ef05136d85
Kugendran <kugendran@me.com>
2025-10-26 08:17:35 +0000

    chore: remove old git helper scripts

--- git_maintenance.sh
+++
@@ -1,9 +0,0 @@
-# One-off cleanup
-git gc --prune=now --aggressive
-git repack -Ad --write-bitmap-index
-git commit-graph write --reachable --changed-paths
-
-# Background maintenance (Git ≥ 2.30)
-git maintenance start
-git maintenance run --task=gc --task=commit-graph --task=prefetch
-
