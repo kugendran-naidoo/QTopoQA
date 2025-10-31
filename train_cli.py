97c66e768f6f2430a9d2f054349bbf925f810236
Kugendran <kugendran@me.com>
2025-10-31 10:34:58 +0000

    repo root version to enable running from anywhere in repo

---
+++ train_cli.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Convenience wrapper for the dynamic training CLI."""
+
+from qtdaqa.new_dynamic_features.model_training.train_cli import main
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
