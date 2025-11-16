d2682d99b167702cab88fa7f23418a0fae7890f5
Kugendran <kugendran@me.com>
2025-11-16 07:34:39 +0000

    added scripts for graph_builder2, model_training and model_inference for automation

---
+++ qtdaqa/new_dynamic_features/model_training/exec_topoqa_10A.sh
@@ -0,0 +1,21 @@
+# Mac
+export TMPDIR=/Volumes/PData/Data/Dev/Github/Repos/phd3/qtopo/QTopoQA/temp
+
+# AWS
+# export TMPDIR=/home/app/phd/github/QTopoQA/temp
+
+export TEMP="$TMPDIR"
+export TMP="$TMPDIR"
+
+export OUTPUT=11d_topoqa_10A
+
+# Mac
+export GRAPH_DIR=/Volumes/PData/Data/Dev/Github/Repos/phd3/qtopo/QTopoQA/qtdaqa/new_dynamic_features/graph_builder2/output/${OUTOUT}/graph_data
+
+# AWS
+# export GRAPH_DIR=/home/app/phd/github/QTopoQA/qtdaqa/new_dynamic_features/graph_builder2/output/${OUTPUT}/graph_data
+
+TMPDIR="$TMPDIR" TEMP="$TEMP" TMP="$TMP" \
+./run_full_pipeline.sh --manifest manifests/run_exhaustive.yaml | 
+tee ${OUTPUT}_run_exhaustive_$(date +%Y%m%d_%H%M%S).log
+
